265b37156f1365894948e821d13808defd2f5bf6
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-05 23:59:00 +0900

    Add Main

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Cafe } from "./Cafe";
 export { default as Header } from "./Common/Header";
 export { default as CustomButton } from "./Common/CustomButton";
 export { default as Create } from "./Create";
+export { default as Main } from "./Main";
