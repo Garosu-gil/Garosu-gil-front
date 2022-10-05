5571c22b291049a188b57623766e8683f6c6d8d9
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-05 19:21:46 +0900

    Update Hello

--- src/components/Hello/index.jsx
+++ src/components/Hello/index.jsx
@@ -1,5 +1,12 @@
+import { css } from "@emotion/react";
 import * as S from "./style";
 
 export default function Hello() {
-  return <S.Hello></S.Hello>;
+  return (
+    <S.Hello
+      css={css`
+        background-color: white;
+      `}
+    ></S.Hello>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,10 @@
-import css from "styled-jsx/css";
 import { Hello } from "../components";
 import useStore from "../stores/storeContainer";
 
 const Home = () => {
   const { num, setNum } = useStore();
 
-  return (
-    <Hello
-      css={css`
-        background-color: purple;
-      `}
-    />
-  );
+  return <Hello />;
 };
 
 export default Home;
