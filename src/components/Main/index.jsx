bccb726ddfc60aeecaf9ec45873e3522a7ff2b82
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-06 12:40:23 +0900

    Update main component

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,7 +1,9 @@
-import { Category } from "../";
+import { Category, CafeCard } from "../";
 import * as S from "./style";
 import * as I from "../../assets";
 import { css } from "@emotion/react";
+import { useState } from "react";
+import Link from "next/link";
 
 const categories = [
   "게임",
@@ -14,7 +16,11 @@ const categories = [
   "기타",
 ];
 
+const mockCafes = ["농구", "a", "a", "a", "a", "a", "a", "a", "a", "a"];
+
 const Main = () => {
+  const [selected, setSelected] = useState(0);
+
   return (
     <S.Main>
       <S.MainContent>
@@ -23,7 +29,13 @@ const Main = () => {
           <S.CategoryContentWrap>
             <S.CategoryList>
               {categories.map((category, index) => (
-                <Category key={index} name={category} />
+                <Category
+                  selected={index === selected}
+                  key={index}
+                  index={index}
+                  name={category}
+                  event={() => setSelected(index)}
+                />
               ))}
             </S.CategoryList>
             <S.CategorySearch placeholder="내가 이야기하고 싶은 주제는?" />
@@ -32,6 +44,17 @@ const Main = () => {
         </S.CategoryWrap>
         <S.CafeWrap>
           <S.Title>운동 카페들</S.Title>
+          <S.CafeList>
+            {mockCafes.map((cafe, index) => (
+              <CafeCard key={index} name={cafe} description={cafe} />
+            ))}
+            <Link href="/create">
+              <S.AddCafe>
+                <S.AddCafePlus>+</S.AddCafePlus>
+                카페 추가
+              </S.AddCafe>
+            </Link>
+          </S.CafeList>
         </S.CafeWrap>
       </S.MainContent>
     </S.Main>
