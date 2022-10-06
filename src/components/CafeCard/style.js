a766027548c5e1ea385e7ed40815d50fbe0429b2
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-06 10:43:42 +0900

    Add CafeCard style

---
+++ src/components/CafeCard/style.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const CafeCard = styled.div`
+  width: 370px;
+  height: 280px;
+  background: #ffffff;
+  box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.1);
+  border-radius: 15px;
+`;
