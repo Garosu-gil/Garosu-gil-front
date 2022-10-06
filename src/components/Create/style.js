ec0f15e3b2c2418ab07dfd5b8c6aea80ae5e5bc0
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-06 20:25:36 +0900

    Update create component style

--- src/components/Create/style.js
+++ src/components/Create/style.js
@@ -56,6 +56,7 @@ export const InputWrap = styled.div`
   display: flex;
   justify-content: space-between;
   flex-direction: column;
+  position: relative;
 `;
 
 export const Input = styled.input`
@@ -67,6 +68,17 @@ export const Input = styled.input`
   border-radius: 15px;
   font-size: 30px;
   padding: 0 20px;
+`;
+
+export const CategoryList = styled.div`
+  width: 100%;
+  height: 130px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background: #ffffff;
+  box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.1);
+  border-radius: 15px;
 `;
 
 export const Textarea = styled.textarea`
@@ -88,4 +100,12 @@ export const Progress = styled.p`
   position: absolute;
   bottom: 20px;
   right: 20px;
+`;
+
+export const InputDescription = styled.p`
+  position: absolute;
+  bottom: 30px;
+  right: 20px;
+  font-size: 30px;
+  color: #a4a4a4;
 `;
