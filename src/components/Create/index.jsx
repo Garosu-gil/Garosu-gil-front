53ca006adb796922962c308eebb2877238e14849
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-06 20:25:30 +0900

    Update create component

--- src/components/Create/index.jsx
+++ src/components/Create/index.jsx
@@ -1,8 +1,32 @@
 import { css } from "@emotion/react";
-import { CustomButton } from "../";
+import { useState } from "react";
+import { CustomButton, Category } from "../";
+import requestApi from "../../libs/axios";
 import * as S from "./style";
 
+const categories = [
+  "게임",
+  "스포츠",
+  "IT",
+  "여행",
+  "취미",
+  "방송",
+  "생활",
+  "기타",
+];
+
 const Create = () => {
+  const [selected, setSelected] = useState("게임");
+
+  const createCafe = async () => {
+    const data = { title: "test", category: selected, explanation: "test" };
+    try {
+      await requestApi({ url: "/create_cafe", method: "post", data: data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <S.Create>
       <S.CreateContent>
@@ -12,10 +36,25 @@ const Create = () => {
             <S.InputWrap>
               <S.SmallTitle>카페 이름</S.SmallTitle>
               <S.Input />
+              <S.InputDescription>
+                카페 (카페는 생략하고 적어주세요.)
+              </S.InputDescription>
             </S.InputWrap>
             <S.InputWrap>
               <S.SmallTitle>카테고리</S.SmallTitle>
-              <S.Input />
+              <S.CategoryList>
+                {categories.map((category, index) => (
+                  <Category
+                    selected={category === selected}
+                    key={index}
+                    index={index}
+                    name={category}
+                    event={() => {
+                      setSelected(category);
+                    }}
+                  />
+                ))}
+              </S.CategoryList>
             </S.InputWrap>
           </S.LeftInputBox>
           <S.RightInputBox>
@@ -29,7 +68,9 @@ const Create = () => {
           height="70px"
           fontSize="32px"
           text="생성"
-          event={() => {}}
+          event={() => {
+            createCafe;
+          }}
           type="submit"
         />
       </S.CreateContent>
