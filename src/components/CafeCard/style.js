f249f8fc4a1f8c94e7493d2fa7132fa580335f1d
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-06 12:38:59 +0900

    Update CafeCard style

--- src/components/CafeCard/style.js
+++ src/components/CafeCard/style.js
@@ -1,9 +1,23 @@
 import styled from "@emotion/styled";
 
 export const CafeCard = styled.div`
-  width: 370px;
-  height: 280px;
   background: #ffffff;
   box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.1);
   border-radius: 15px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 20px;
+`;
+
+export const Name = styled.p`
+  font-weight: 500;
+  font-size: 30px;
+`;
+
+export const Description = styled.p`
+  font-size: 15px;
+  position: relative;
+  top: 30px;
 `;
