ef21fb92396205188ae31d39c9298fd5f571bc33
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-06 12:39:28 +0900

    Update Category style

--- src/components/Category/style.js
+++ src/components/Category/style.js
@@ -1,5 +1,18 @@
 import styled from "@emotion/styled";
 
 export const Category = styled.div`
-  height: 100px;
+  width: 90px;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 20px;
+  padding: 10px 0;
+  cursor: pointer;
+`;
+
+export const Name = styled.p`
+  font-weight: 700;
+  font-size: 16px;
 `;
