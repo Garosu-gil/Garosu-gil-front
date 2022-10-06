bab06cf089c53be83ceb63b1425e1e4c1e1ea462
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-06 22:29:00 +0900

    Resolve problem

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -1,25 +1,22 @@
 import * as S from "./style";
 import * as I from "../../../assets";
 import { css } from "@emotion/react";
-import Link from "next/link";
 
-const Header = ({ cafeName }) => {
+const Header = () => {
   return (
     <S.Header>
-      <Link href="/">
-        <S.Title>
-          <S.TitleDesc>너만의 카페를 만들어 봐</S.TitleDesc>
-          <S.LogoWrapper>
-            <S.Logo>가로수길</S.Logo>
-            <I.Logo
-              css={css`
-                background-color: black;
-              `}
-            />
-          </S.LogoWrapper>
-        </S.Title>
-      </Link>
-      {cafeName && <S.CafeName>{cafeName}</S.CafeName>}
+      <S.Title>
+        <S.TitleDesc>너만의 카페를 만들어 봐</S.TitleDesc>
+        <S.LogoWrapper>
+          <S.Logo>가로수길</S.Logo>
+          <I.Logo
+            css={css`
+              background-color: black;
+            `}
+          />
+        </S.LogoWrapper>
+      </S.Title>
+      <S.CafeName>농구카페</S.CafeName>
     </S.Header>
   );
 };
