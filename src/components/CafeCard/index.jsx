4ad979b61a93ef0248938bd3cea04ed07536cc69
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-06 12:38:41 +0900

    Update CafeCard

--- src/components/CafeCard/index.jsx
+++ src/components/CafeCard/index.jsx
@@ -1,7 +1,15 @@
 import * as S from "./style";
+import Link from "next/link";
 
-const CafeCard = ({ name }) => {
-  return <S.CafeCard>{name} 카페</S.CafeCard>;
+const CafeCard = ({ name, description }) => {
+  return (
+    <Link href={`/cafe/${name}`}>
+      <S.CafeCard>
+        <S.Name>{name} 카페</S.Name>
+        <S.Description>{description}</S.Description>
+      </S.CafeCard>
+    </Link>
+  );
 };
 
 export default CafeCard;
